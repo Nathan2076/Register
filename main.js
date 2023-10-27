88ffb2bd99ae0a53747220887ba5e183b145d19c
Nathan2076 <68337317+Nathan2076@users.noreply.github.com>
2023-10-27 09:05:50 -0300

    Add CEP validation

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
+"use strict";  // Modo restrito
+// https://viacep.com.br
+
+// Função para limpar formulário
+
+/*const limparForm = () => {
+    document.getElementById("address").value = "";
+    document.getElementById("neighborhood").value = "";
+    document.getElementById("city").value = "";
+    document.getElementById("state").value = "";
+}*/
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+// Responsável pelo preenchimento do formulário
+
+const preencherForm = (endereco) => {
+    document.getElementById("address").value = endereco.logradouro;
+    document.getElementById("neighborhood").value = endereco.bairro;
+    document.getElementById("city").value = endereco.localidade;
+    document.getElementById("state").value = endereco.uf;
+}
+
+// Função para consumo de API da Via CEP
+
+const pesquisarCep = async() => {
+    //limparForm();
+    const url = `https://viacep.com.br/ws/${cep.value}/json/`;
+
+    if (cepValido(cep.value)) {
+        const dados = await fetch(url);  // Espera
+        const address = await dados.json();  // Retorna dados no formato JSON
+        
+        if (address.hasOwnProperty("erro")) {
+            alert("CEP não encontrado");
+        } else {
+            preencherForm(address);
+        }
+    } else {
+        alert("CEP incorreto");
+    }
+}
+
+// Adiciona um evento DOM no input do CEP
+
+document.getElementById("cep").addEventListener("focusout", pesquisarCep);
+
 function validateForm(e)
 {
     e.preventDefault();
@@ -30,7 +78,8 @@ function validateForm(e)
         return;
     }
     
-    alert(`Nome: ${name}\nEmail: ${email}\nTelefone: ${phone}\n`
+    alert("Cadastro realizado com sucesso!\n\n"
+        + `Nome: ${name}\nEmail: ${email}\nTelefone: ${phone}\n`
         + `Data de nascimento: ${birthdate}\nCPF: ${cpf}\nCEP: ${cep}\n`
         + `Endereço: ${address}\nNúmero: ${number}\n`
         + `Complemento: ${complement}\nBairro: ${neighborhood}\n`
